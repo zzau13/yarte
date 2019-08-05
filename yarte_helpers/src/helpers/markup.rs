013aa85d456a491adb12f676cbc83dea3f67074a
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-08-05 21:06:47 +0200

    Improve performance of markup helper

--- yarte_helpers/src/helpers/markup.rs
+++ yarte_helpers/src/helpers/markup.rs
@@ -3,36 +3,39 @@ use std::fmt::{self, Display};
 
 use v_htmlescape::escape;
 
-pub trait Render {
+pub trait Render: Display {
     fn render(&self, f: &mut fmt::Formatter) -> fmt::Result;
 }
 
-impl<'a, T: Render + ?Sized> Render for &'a T {
-    fn render(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        (*self).render(f)
-    }
+macro_rules! str_display {
+    ($($ty:ty)*) => {
+        $(
+            impl Render for &$ty {
+                fn render(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                    escape(self).fmt(f)
+                }
+            }
+        )*
+    };
 }
 
-impl<T: Render> Render for Option<T> {
-    fn render(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Some(t) => t.render(f),
-            None => Ok(()),
-        }
-    }
+#[rustfmt::skip]
+str_display!(str &str &&str);
+
+macro_rules! string_display {
+    ($($ty:ty)*) => {
+        $(
+            impl Render for $ty {
+                fn render(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                    escape(self.as_str()).fmt(f)
+                }
+            }
+        )*
+    };
 }
 
-impl Render for str {
-    fn render(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        escape(self).fmt(f)
-    }
-}
-
-impl Render for String {
-    fn render(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.as_str().render(f)
-    }
-}
+#[rustfmt::skip]
+string_display!(String &String &&String);
 
 macro_rules! raw_display {
     ($($ty:ty)*) => {
@@ -52,4 +55,14 @@ raw_display! {
     u8 u16 u32 u64 u128 usize
     i8 i16 i32 i64 i128 isize
     f32 f64
+    &bool
+    &char
+    &u8 &u16 &u32 &u64 &u128 &usize
+    &i8 &i16 &i32 &i64 &i128 &isize
+    &f32 &f64
+    &&bool
+    &&char
+    &&u8 &&u16 &&u32 &&u64 &&u128 &&usize
+    &&i8 &&i16 &&i32 &&i64 &&i128 &&isize
+    &&f32 &&f64
 }
