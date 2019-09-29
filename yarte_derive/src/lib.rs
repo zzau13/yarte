f9d26e93c68f0ef750b674a6f8b6430a45373f04
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-09-29 08:37:16 +0200

    Fix clippy::map_entry

--- yarte_derive/src/lib.rs
+++ yarte_derive/src/lib.rs
@@ -69,7 +69,6 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 }
 
 fn read(path: PathBuf, src: String, config: &Config) -> BTreeMap<PathBuf, String> {
-    #[allow(clippy::map_entry)]
     fn _read(
         path: PathBuf,
         src: String,
@@ -77,35 +76,31 @@ fn read(path: PathBuf, src: String, config: &Config) -> BTreeMap<PathBuf, String
         visited: &mut BTreeMap<PathBuf, String>,
         stack: &mut Vec<u64>,
     ) {
-        if !visited.contains_key(&path) {
-            stack.push(calculate_hash(&path));
+        stack.push(calculate_hash(&path));
 
-            let partials = parse_partials(&src)
-                .iter()
-                .map(|n| match n {
-                    Node::Partial(_, partial, _) => config.resolve_partial(&path, partial),
-                    _ => unreachable!(),
-                })
-                .collect::<BTreeSet<_>>();
+        let partials = parse_partials(&src)
+            .iter()
+            .map(|n| match n {
+                Node::Partial(_, partial, _) => config.resolve_partial(&path, partial),
+                _ => unreachable!(),
+            })
+            .collect::<BTreeSet<_>>();
 
-            for partial in &partials {
-                if stack.contains(&calculate_hash(partial)) {
-                    panic!(
-                        "Partial cyclic dependency {:?} in template {:?}",
-                        partial, path
-                    );
-                }
-            }
+        visited.insert(path.clone(), src);
 
-            visited.insert(path, src);
-
-            for partial in partials {
+        for partial in partials {
+            if !visited.contains_key(&partial) {
                 let src = get_source(partial.as_path());
                 _read(partial, src, config, visited, stack);
+            } else if stack.contains(&calculate_hash(&partial)) {
+                panic!(
+                    "Partial cyclic dependency {:?} in template {:?}",
+                    partial, path
+                );
             }
-
-            stack.pop();
         }
+
+        stack.pop();
     }
 
     let mut visited = BTreeMap::new();
