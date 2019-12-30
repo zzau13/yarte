6a8b8e758d8783e468cb50f16ea344480859ab17
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-12-31 00:28:05 +0100

    Add black box poc of tree builder and tree diff

--- yarte_wasm_app/src/lib.rs
+++ yarte_wasm_app/src/lib.rs
@@ -256,11 +256,174 @@ mod test {
     #[derive(Default)]
     struct Test {
         c: Rc<AtomicUsize>,
+        any: usize,
+        it: Vec<usize>,
         black_box: <Self as App>::BlackBox,
     }
 
     impl App for Test {
-        type BlackBox = ();
+        type BlackBox = BlackBox;
+    }
+
+    /// PoC tree builder and diff
+    /// Tree is a tree of flow with static bool slices for expressions and IfElse flow
+    ///  `app.black_box.t_root & [bool; N] != 0`
+    /// For iterables vector of bool slices allocated in black box
+    ///  `app.black_box.t_children_I[J] & [bool; M] != 0`
+    /// management of children will control by `__render`
+    /// and macros push or pop or replace
+    /// or manual control of black box
+    ///
+    /// BlackBox trait Tree control
+    ///     - render_all:
+    ///     - ignore_next_render ?:
+    /// public fields in BlackBox for Dom references
+    ///
+    /// Construct in base of used variables in templates
+    ///
+    /// this macro is construct in derive
+    #[derive(Debug, PartialEq)]
+    struct BlackBox {
+        t_root: u8,
+        t_children_0: Vec<bool>,
+    }
+
+    impl Default for BlackBox {
+        fn default() -> BlackBox {
+            BlackBox {
+                t_root: 0xFF,
+                t_children_0: vec![],
+            }
+        }
+    }
+
+    impl BlackBox {
+        fn set_zero(&mut self) {
+            self.t_root = 0;
+            for child in self.t_children_0.iter_mut() {
+                *child = false;
+            }
+        }
+    }
+
+    #[macro_export]
+    macro_rules! set_any {
+        ($app:ident, $value:expr) => {
+            // fields, index will set in derive
+            $app.black_box.t_root |= 1 << 1;
+            $app.any = $value;
+        };
+    }
+
+    #[macro_export]
+    macro_rules! set_it {
+        ($app:ident, $value:expr) => {
+            // fields, index will set in derive
+            let value = $value;
+            $app.black_box.t_root |= 1 << 2;
+            $app.black_box.t_children_0 = vec![true; value.len()];
+            $app.it = value;
+        };
+    }
+
+    /// For template
+    /// ```hbs
+    /// <h1>{{ c }} {{ any }}</h1>
+    /// <div>
+    ///     {{ each it }}
+    ///         {{ this + 2 }}
+    ///         <br/>
+    ///     {{/ each }}
+    /// </div>
+    /// ```
+    /// ```
+    /// struct BlackBox {
+    ///     t_root: bool,
+    ///     t_children_0: Vec<bool>,
+    ///     ...
+    /// }
+    /// ```
+    ///
+    #[macro_export]
+    macro_rules! push_it {
+        ($app:ident, $value:expr) => {
+            // fields, index will set in derive
+            $app.black_box.t_root |= 1 << 2;
+            $app.black_box.t_children_0.push(true);
+            $app.it.push($value);
+        };
+    }
+
+    #[macro_export]
+    macro_rules! pop_it {
+        ($app:ident) => {{
+            // fields, index will set in derive
+            $app.black_box.t_root |= 1 << 2;
+            $app.black_box.t_children_0.pop();
+            $app.it.pop()
+        }};
+    }
+
+    #[macro_export]
+    macro_rules! set_it_index {
+        ($app:ident [$i:expr] $value:expr) => {
+            // fields, index will set in derive
+            $app.black_box.t_root |= 1 << 2;
+            $app.black_box
+                .t_children_0
+                .get_mut($i)
+                .map(|x| *x = true)
+                .and_then(|_| $app.it.get_mut($i).map(|x| *x = $value))
+        };
+    }
+
+    struct MsgTree(usize);
+
+    impl Message for MsgTree {}
+
+    impl Handler<MsgTree> for Test {
+        fn handle(&mut self, msg: MsgTree, _mb: &Mailbox<Self>) {
+            self.black_box.set_zero();
+            // after first render
+            let expected = BlackBox {
+                t_root: 0,
+                t_children_0: vec![],
+            };
+            assert_eq!(self.black_box, expected);
+            set_any!(self, msg.0);
+            set_it!(self, vec![1, 2, 3, 4]);
+            let expected = BlackBox {
+                t_root: 6,
+                t_children_0: vec![true, true, true, true],
+            };
+            assert_eq!(self.black_box, expected);
+            self.black_box.set_zero();
+            push_it!(self, 5);
+            let expected = BlackBox {
+                t_root: 4,
+                t_children_0: vec![false, false, false, false, true],
+            };
+            assert_eq!(self.black_box, expected);
+            self.black_box.set_zero();
+            let _ = pop_it!(self);
+            let expected = BlackBox {
+                t_root: 4,
+                t_children_0: vec![false, false, false, false],
+            };
+            assert_eq!(self.black_box, expected);
+            self.black_box.set_zero();
+            let expected = BlackBox {
+                t_root: 0,
+                t_children_0: vec![false, false, false, false],
+            };
+            assert_eq!(self.black_box, expected);
+            set_it_index!(self [1] 6);
+            let expected = BlackBox {
+                t_root: 4,
+                t_children_0: vec![false, true, false, false],
+            };
+            assert_eq!(self.black_box, expected)
+        }
     }
 
     struct Msg(usize);
@@ -339,5 +502,6 @@ mod test {
         });
         addr.send(Reset);
         assert_eq!(c2.load(Ordering::Relaxed), 0);
+        addr.send(MsgTree(0))
     }
 }
