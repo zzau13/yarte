88067f400e6bec927870e4f86b90549e02441258
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-01-08 03:25:23 +0100

    Add coverage

--- yarte/tests/html.rs
+++ yarte/tests/html.rs
@@ -138,3 +138,41 @@ fn test_pre() {
         t.call().unwrap()
     )
 }
+
+#[derive(Template)]
+#[template(path = "html/section.hbs")]
+struct SectionTemplate {
+    flag: bool,
+}
+
+#[test]
+fn test_section() {
+    let t = SectionTemplate { flag: false };
+    let expected = "<section id=\"embedded\"><header><h1>Embedded content</h1></header>\
+        <article id=\"embedded__images\"><header><h2>Images</h2></header><div>\
+        <h3>No <code>&lt;figure&gt;</code> element</h3><p>\
+        <img src=\"http://placekitten.com/480/480\" alt=\"Image alt text\"></p>\
+        <h3>Wrapped in a <code>&lt;figure&gt;</code> element, no <code>&lt;figcaption&gt;</code>\
+        </h3><figure><img src=\"http://placekitten.com/420/420\" alt=\"Image alt text\"></figure>\
+        <h3>Wrapped in a <code>&lt;figure&gt;</code> element, with a <code>&lt;figcaption&gt;\
+        </code></h3><figure><img src=\"http://placekitten.com/420/420\" alt=\"Image alt text\">\
+        <figcaption>Here is a caption for this image.</figcaption></figure></div><footer><p>\
+        <a href=\"#top\">[Top]</a></p></footer></article><article id=\"embedded__audio\"><header>\
+        <h2>Audio</h2></header><div><audio controls>audio</audio></div><footer><p><a href=\"#top\">\
+        [Top]</a></p></footer></article><article id=\"embedded__video\"><header><h2>Video</h2>\
+        </header><div><video controls>video</video></div><footer><p><a href=\"#top\">[Top]</a>\
+        </p></footer></article><article id=\"embedded__canvas\"><header><h2>Canvas</h2></header>\
+        <div><canvas>canvas</canvas></div><footer><p><a href=\"#top\">[Top]</a></p></footer>\
+        </article><article id=\"embedded__meter\"><header><h2>Meter</h2></header><div>\
+        <meter value=\"2\" min=\"0\" max=\"10\">2 out of 10</meter></div><footer><p>\
+        <a href=\"#top\">[Top]</a></p></footer></article><article id=\"embedded__progress\">\
+        <header><h2>Progress</h2></header><div><progress>progress</progress></div><footer><p>\
+        <a href=\"#top\">[Top]</a></p></footer></article><article id=\"embedded__svg\"><header>\
+        <h2>Inline SVG</h2></header><div><svg width=\"100px\" height=\"100px\">\
+        <circle cx=\"100\" cy=\"100\" r=\"100\" fill=\"#1fa3ec\"></circle></svg></div><footer>\
+        <p><a href=\"#top\">[Top]</a></p></footer></article><article id=\"embedded__iframe\">\
+        <header><h2>IFrame</h2></header><div><iframe src=\"index.html\" height=\"300\"></iframe>\
+        </div><footer><p><a href=\"#top\">[Top]</a></p></footer></article></section>";
+
+    assert_eq!(expected, t.call().unwrap())
+}
