081ceb2ef93a6043c90e439e52e41dc518ddd49e
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-01-12 04:06:58 +0100

    Add macro mark flow

--- yarte_wasm_app/benches/src/handler.rs
+++ yarte_wasm_app/benches/src/handler.rs
@@ -33,14 +33,21 @@ impl NonKeyed {
     fn run_n(&mut self, n: usize) {
         let update_n = min(n, self.data.len());
 
-        for i in 0..update_n {
-            let mut row = &mut self.data[i];
+        // ?? #macro mark_data_update(for i in
+        for (i, (dom, row)) in self
+            .tbody_children
+            .iter_mut()
+            .zip(self.data.iter_mut())
+            .enumerate()
+            .take(update_n)
+        {
             row.id = self.id + i as u32;
             row.label = (*ZIPPED.choose(&mut self.rng).unwrap()).to_string();
-            self.tbody_children[i].t_root |= 0b0000_0011
+            // or #macro mark_data_
+            dom.t_root = 0xFF;
+            // /macro
         }
-
-        self.id += update_n as u32;
+        // /macro
 
         for i in update_n..n {
             self.data.push(Row {
@@ -49,9 +56,16 @@ impl NonKeyed {
             });
         }
 
-        self.id += (n - update_n) as u32;
+        // #macro mark_data
+        self.t_root |= 0b0000_0001;
+        // /macro
+
+        self.id += n as u32;
+
+        // #macro set_selected
         self.selected = None;
-        self.t_root |= 0b0000_0011;
+        self.t_root |= 0b0000_0010;
+        // /macro
     }
 }
 
@@ -88,8 +102,11 @@ impl Handler<Append> for NonKeyed {
                 label: (*ZIPPED.choose(&mut self.rng).unwrap()).to_string(),
             })
         }
+        // id its not present in template and haven't a marker
         self.id += n;
+        // #macro mark_data
         self.t_root |= 0b0000_0001;
+        // /macro
     }
 }
 
@@ -99,12 +116,21 @@ impl Message for Update {}
 
 impl Handler<Update> for NonKeyed {
     fn handle(&mut self, _msg: Update, _mb: &Addr<Self>) {
-        for (row, dom) in self.data.iter_mut().zip(self.tbody_children.iter_mut()).step_by(10) {
+        for (row, dom) in self
+            .data
+            .iter_mut()
+            .zip(self.tbody_children.iter_mut())
+            .step_by(10)
+        {
             row.label.push_str(" !!!");
+            // #macro mark_data_label
             dom.t_root |= 0b0000_0001;
+            // /macro
         }
 
+        // #macro mark_data
         self.t_root |= 0b0000_0001;
+        // /macro
     }
 }
 
@@ -114,8 +140,10 @@ impl Message for Clear {}
 
 impl Handler<Clear> for NonKeyed {
     fn handle(&mut self, _msg: Clear, _mb: &Addr<Self>) {
+        // #macro data_swap
         self.data.clear();
         self.t_root |= 0b0000_0001;
+        // /macro
     }
 }
 
@@ -129,10 +157,12 @@ impl Handler<Swap> for NonKeyed {
             return;
         }
 
+        // #macro data_swap
         self.data.swap(1, 998);
         self.tbody_children[1].t_root = 0xFF;
         self.tbody_children[998].t_root = 0xFF;
         self.t_root |= 0b0000_0001;
+        // /macro
     }
 }
 
@@ -142,16 +172,20 @@ impl Message for Select {}
 
 impl Handler<Select> for NonKeyed {
     fn handle(&mut self, msg: Select, _mb: &Addr<Self>) {
-        self.t_root |= 0b0000_0010;
-
         if let Some(t) = self.selected {
             if t == msg.0 {
+                // #macro set_selected
                 self.selected = None;
+                self.t_root |= 0b0000_0010;
+                // /macro
                 return;
             }
         }
 
+        // #macro set_selected
         self.selected = Some(msg.0);
+        self.t_root |= 0b0000_0010;
+        // /macro
     }
 }
 
@@ -163,20 +197,23 @@ impl Handler<Delete> for NonKeyed {
     fn handle(&mut self, msg: Delete, _mb: &Addr<Self>) {
         if let Some(selected) = self.selected {
             if msg.0 == selected {
+                // #macro set_selected
                 self.selected = None;
                 self.t_root |= 0b0000_0010;
+                // /macro
             }
         }
         self.data
             .iter()
             .position(|x| x.id == msg.0)
             .map(|x| {
+                // #macro remove_data(_self, i: usize)
+                self.tbody.children().item(x as u32).unwrap_throw().remove();
+                self.tbody_children.remove(x);
                 self.data.remove(x);
-                for x in self.tbody_children[x..].iter_mut() {
-                    x.t_root = 0xFF;
-                }
+                self.t_root |= 0b0000_0001;
+                // /macro
             })
             .unwrap_throw();
-        self.t_root |= 0b0000_0001;
     }
 }
