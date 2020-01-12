7f3a6bf94d47ccec15039b15270050799a67fbf6
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-01-12 21:19:32 +0100

    Fix wasm Each for iterator

--- yarte_wasm_app/benches/src/handler.rs
+++ yarte_wasm_app/benches/src/handler.rs
@@ -208,7 +208,9 @@ impl Handler<Delete> for NonKeyed {
             .position(|x| x.id == msg.0)
             .map(|x| {
                 // #macro remove_data(_self, i: usize)
-                self.tbody.remove_child(&self.tbody_children.remove(x).root).unwrap_throw();
+                self.tbody
+                    .remove_child(&self.tbody_children.remove(x).root)
+                    .unwrap_throw();
                 self.data.remove(x);
                 self.t_root |= 0b0000_0001;
                 // /macro
