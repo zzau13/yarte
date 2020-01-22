fe677c0288973376220e869678e0eb8d4b64a3ea
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-01-22 04:51:08 +0100

    Add inner attributes

--- test_wasm/tests/wasm.rs
+++ test_wasm/tests/wasm.rs
@@ -22,14 +22,27 @@ struct Fortune {
 struct Test {
     fortunes: Vec<Fortune>,
     head: String,
+    #[inner]
+    bar: usize,
+    #[inner("foo")]
+    barr: usize,
     black_box: <Self as Template>::BlackBox,
 }
 
+#[inline]
+fn foo() -> usize {
+    1
+}
+
+#[inline]
 fn any_msg(_app: &mut Test, _0: usize, _addr: &yarte::Addr<Test>) {}
+#[inline]
 fn new_point(_app: &mut Test, _a: usize, _b: usize, _addr: &yarte::Addr<Test>) {}
+#[inline]
 fn ping(_app: &mut Test, _addr: &yarte::Addr<Test>) {}
 mod path {
     use super::*;
+    #[inline]
     pub(super) fn func(_app: &mut Test, _addr: &yarte::Addr<Test>) {}
 }
 
