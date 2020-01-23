eddca06dd0f23cc78eee4f88c9f8bf81fd18698e
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-01-23 23:08:20 +0100

    Prettify each

--- yarte_codegen/src/wasm/client/each.rs
+++ yarte_codegen/src/wasm/client/each.rs
@@ -2,7 +2,7 @@ use std::mem;
 
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::parse2;
+use syn::{parse2, Expr, Ident};
 
 use yarte_dom::dom::{Each, ExprId};
 
@@ -20,6 +20,7 @@ impl<'a> WASMCodeGen<'a> {
         }: &Each,
         insert_point: TokenStream,
     ) {
+        // TODO: add Each to tree map
         let vars = self.tree_map.get(&id).cloned().unwrap_or_default();
         let parent = self.parent_node();
 
@@ -33,6 +34,7 @@ impl<'a> WASMCodeGen<'a> {
         let ty = format_ident!("Component{}", id);
         let name = format_ident!("ytable_{}", id);
         let name_elem = format_ident!("ytable_dom_{}", id);
+
         self.add_black_box_t_root();
         self.black_box.push(BlackBox {
             doc: "Each root dom element".to_string(),
@@ -44,31 +46,46 @@ impl<'a> WASMCodeGen<'a> {
             name: name.clone(),
             ty: parse2(quote!(yarte::web::Element)).unwrap(),
         });
+
         let black_box = self.black_box(&ty);
-        self.helpers.extend(black_box);
-        self.black_box = old_b;
+
+        // TODO get current black_box
         let bb_name = self.get_black_box_ident();
-        let table = quote!(self.#bb_name.#name);
-        let render = self.empty_buff();
+        let c_bb = quote!(self.#bb_name);
+        let table = quote!(#c_bb.#name);
+        let elem = quote!(#c_bb.#name_elem);
         let dom = format_ident!("dom_{}", id);
+
+        old_buff.push((vars.clone(), self.render_each(table, args, expr, elem, dom)));
+
+        self.helpers.extend(black_box);
+
+        self.black_box = old_b;
+        self.buff_render = old_buff;
+    }
+
+    fn render_each(&mut self, table: TokenStream, args: &Expr, expr: &Expr, elem: TokenStream, dom: Ident) -> TokenStream {
         let new = TokenStream::new();
-        old_buff.push((
-            vars.clone(),
-            quote! {
+        let render = self.empty_buff();
+        // TODO get parents dependency
+        let check = quote!(d.t_root != 0);
+
+        quote! {
             let dom_len = #table.len();
             let data_len = #args.size_hint().0;
             if data_len == 0 {
-                self.#name_elem.set_text_content(None);
+                #elem.set_text_content(None);
                 #table.clear()
             } else {
                 for (#dom, #expr) in #table
                     .iter_mut()
                     .zip(#args)
-                    .filter(|(dom, _)| dom.t_root != 0) { #render }
+                    .filter(|(d, _)| #check)
+                    { #render }
 
                 if dom_len < data_len {
                     for row in self.data.iter().skip(dom_len) {
-                        #table.push(new_row!(row, self.tr, mb, self.tbody));
+                        #table.push(#new);
                     }
                 } else {
                     for dom in #table.drain(data_len..) {
@@ -76,8 +93,6 @@ impl<'a> WASMCodeGen<'a> {
                     }
                 }
             }
-            },
-        ));
-        self.buff_render = old_buff;
+            }
     }
 }
