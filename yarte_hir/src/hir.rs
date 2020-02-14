4e83e5bfd9b4f0f3fe4ff1c3da5bff7609053af2
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-02-14 16:31:26 +0100

    Add Display error kind

---
+++ yarte_hir/src/hir.rs
@@ -0,0 +1,24 @@
+/// High level intermediate representation after lowering Ast
+#[derive(Debug, Clone, PartialEq)]
+pub enum HIR {
+    Lit(String),
+    Expr(Box<syn::Expr>),
+    Safe(Box<syn::Expr>),
+    Each(Box<Each>),
+    IfElse(Box<IfElse>),
+    Local(Box<syn::Local>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfElse {
+    pub ifs: (syn::Expr, Vec<HIR>),
+    pub if_else: Vec<(syn::Expr, Vec<HIR>)>,
+    pub els: Option<Vec<HIR>>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Each {
+    pub args: syn::Expr,
+    pub body: Vec<HIR>,
+    pub expr: syn::Expr,
+}
