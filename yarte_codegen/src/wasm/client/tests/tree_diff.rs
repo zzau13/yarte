10cbe67911dd3fbbf21ed587c38cdbf0c4a377c6
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-02-17 18:09:10 +0100

    [wasm] Fix render priority

--- yarte_codegen/src/wasm/client/tests/tree_diff.rs
+++ yarte_codegen/src/wasm/client/tests/tree_diff.rs
@@ -28,7 +28,7 @@ fn test_diff_u16() {
                 }
 
                 if yarte::YNumber::neq_zero(self.black_box.t_root & #diff) {
-                    self.black_box.__ynode__0.set_text_content(Some(&format!("{}{}{}{}{}{}{}{}{}", self.foo, self.f1, self.f2, self.f3, self.f4, self.f5, self.f6, self.f7, self.f8) ));
+                    self.black_box.__ynode__0.set_text_content(Some(&format!("{}{}{}{}{}{}{}{}{}", self.foo, self.f1, self.f2, self.f3, self.f4, self.f5, self.f6, self.f7, self.f8)));
                 }
 
                 self.black_box.t_root = yarte::YNumber::zero();
@@ -78,6 +78,104 @@ fn test_diff_u16() {
                 Self {
                     black_box: TestBlackBox {
                         __ynode__0: __ynode__0,
+                        t_root: yarte::YNumber::zero()
+                    }
+                }
+            }
+        }
+    }
+        .to_string();
+
+    assert_eq!(tokens(src), expected)
+}
+
+#[test]
+fn test_diff_u16_1() {
+    let src = r#"
+    <!doctype html><html><body>
+    <div>{{ foo }}{{f1}}{{f2}}{{f3}}{{f4}}{{f5}}{{f6}}{{f7}}{{f8}}</div>
+    <div>{{f9}}</div>
+    </body></html>"#;
+
+    let diff_0 = 959u16;
+    let diff_1 = 64u16;
+    assert_eq!(diff_0 & diff_1, 0);
+    let expected = quote! {
+        #[wasm_bindgen]
+        extern "C" {
+            fn get_state() -> String;
+        }
+
+        impl yarte::Template for Test {
+            type BlackBox = TestBlackBox;
+            type Message = Msg;
+
+            #[doc(hidden)]
+            #[inline]
+            fn __render (&mut self, __addr: &yarte::Addr<Self>) {
+                if self.black_box.t_root == <u16 as yarte::YNumber>::zero() {
+                    return;
+                }
+
+                if yarte::YNumber::neq_zero(self.black_box.t_root & #diff_0) {
+                    self.black_box.__ynode__0.set_text_content(Some(&format!("{}{}{}{}{}{}{}{}{}", self.foo, self.f1, self.f2, self.f3, self.f4, self.f5, self.f6, self.f7, self.f8)));
+                }
+
+                if yarte::YNumber::neq_zero(self.black_box.t_root & #diff_1) {
+                    self.black_box.__ynode__1.set_text_content(Some(&format!("{}", self.f9)));
+                }
+
+                self.black_box.t_root = yarte::YNumber::zero();
+            }
+
+            #[doc(hidden)]
+            #[inline]
+            fn __hydrate (&mut self, __addr: &yarte::Addr<Self>) {
+                let __ybody = yarte::web::window()
+                    .unwrap_throw()
+                    .document()
+                    .unwrap_throw()
+                    .body()
+                    .unwrap_throw();
+            }
+
+            #[doc(hidden)]
+            fn __dispatch(&mut self, __msg: Self::Message, __addr: &yarte::Addr<Self>) {
+                use Msg::*;
+                match __msg {
+                    Foo => foo(self, __addr)
+                }
+            }
+        }
+
+        pub enum Msg {
+            Foo,
+        }
+
+        #[derive(Default, serde::Deserialize)]
+        struct TestInitialState { }
+
+        #[doc = "Internal elements and difference tree"]
+        pub struct TestBlackBox {
+            #[doc = "Yarte Node element" ]
+            pub __ynode__0: yarte::web::Element,
+            #[doc = "Yarte Node element" ]
+            pub __ynode__1: yarte::web::Element,
+            #[doc = "Difference tree"]
+            pub t_root: u16
+        }
+
+        impl std::default::Default for Test {
+            fn default() -> Self {
+                let TestInitialState { } = yarte::from_str(&get_state()).unwrap_or_default();
+                let doc = yarte::web::window().unwrap_throw().document().unwrap_throw();
+                let __ybody = doc.body().unwrap_throw();
+                let __ynode__0 = __ybody.first_element_child().unwrap_throw();
+                let __ynode__1 = __ynode__0.next_element_sibling().unwrap_throw();
+                Self {
+                    black_box: TestBlackBox {
+                        __ynode__0: __ynode__0,
+                        __ynode__1: __ynode__1,
                         t_root: yarte::YNumber::zero()
                     }
                 }
