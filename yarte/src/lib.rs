65250da820790779552bf99d9edb100fd27a62b0
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-02-21 22:47:22 +0100

    Remove yarte_template crate

--- yarte/src/lib.rs
+++ yarte/src/lib.rs
@@ -9,10 +9,30 @@
 //!
 //! [Yarte book](https://yarte.netlify.com)
 //!
+use std::fmt::{self, Write};
 
 pub use yarte_derive::Template;
 pub use yarte_helpers::{helpers::Render, recompile, Error, Result};
-pub use yarte_template::Template;
+
+/// Template trait, will implement by derive like `Display` or `actix_web::Responder` (with feature)
+///
+pub trait Template: fmt::Display {
+    /// which will write this template
+    fn call(&self) -> Result<String> {
+        let mut buf = String::with_capacity(Self::size_hint());
+        write!(buf, "{}", self).map(|_| buf)
+    }
+
+    /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+    #[cfg(any(feature = "mime", feature = "with-actix-web"))]
+    fn mime() -> &'static str
+    where
+        Self: Sized;
+
+    /// Approximation of output size used in method `call`.
+    /// Yarte implements an heuristic algorithm of allocation.
+    fn size_hint() -> usize;
+}
 
 #[cfg(feature = "with-actix-web")]
 pub mod aw {
