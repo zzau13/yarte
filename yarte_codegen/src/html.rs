45ad96f53cb26de635a1664f22b733a06873bbfd
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-03-14 14:38:54 +0100

    Add yarte_format crate at 0.1.0

--- yarte_codegen/src/html.rs
+++ yarte_codegen/src/html.rs
@@ -1,44 +1,47 @@
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 
 use yarte_dom::DOMFmt;
 
 use super::{CodeGen, EachCodeGen, IfElseCodeGen, HIR};
 
-pub struct HTMLCodeGen;
+pub struct HTMLCodeGen(pub &'static str);
 
 impl EachCodeGen for HTMLCodeGen {}
 
 impl IfElseCodeGen for HTMLCodeGen {}
 impl CodeGen for HTMLCodeGen {
     fn gen(&mut self, v: Vec<HIR>) -> TokenStream {
-        gen(self, v)
+        let parent = self.0;
+        gen(self, v, parent)
     }
 }
 
-pub struct HTMLMinCodeGen;
+pub struct HTMLMinCodeGen(pub &'static str);
 impl EachCodeGen for HTMLMinCodeGen {}
 impl IfElseCodeGen for HTMLMinCodeGen {}
 
 impl CodeGen for HTMLMinCodeGen {
     fn gen(&mut self, v: Vec<HIR>) -> TokenStream {
+        let parent = self.0;
         let dom: DOMFmt = v.into();
-        gen(self, dom.0)
+        gen(self, dom.0, parent)
     }
 }
 
-fn gen<C>(codegen: &mut C, v: Vec<HIR>) -> TokenStream
+fn gen<C>(codegen: &mut C, v: Vec<HIR>, parent: &str) -> TokenStream
 where
     C: CodeGen + EachCodeGen + IfElseCodeGen,
 {
     let mut tokens = TokenStream::new();
+    let parent = format_ident!("{}", parent);
     for i in v {
         use HIR::*;
         tokens.extend(match i {
             Local(a) => quote!(#a),
             Lit(a) => quote!(_fmt.write_str(#a)?;),
-            Safe(a) => quote!(::std::fmt::Display::fmt(&(#a), _fmt)?;),
-            Expr(a) => quote!(::yarte::Render::render(&(#a), _fmt)?;),
+            Safe(a) => quote!(std::fmt::Display::fmt(&(#a), _fmt)?;),
+            Expr(a) => quote!(#parent::Render::render(&(#a), _fmt)?;),
             Each(a) => codegen.gen_each(*a),
             IfElse(a) => codegen.gen_if_else(*a),
         })
--- yarte_format/src/lib.rs
+++ yarte_format/src/lib.rs
@@ -1,16 +1,18 @@
-#![cfg(feature = "nightly")]
+#![cfg(nightly)]
 #![feature(proc_macro_hygiene)]
 
-/// Adapted from [`fomat`](https://github.com/krdln/fomat-macros)
+/// Adapted and improve from [`fomat`](https://github.com/krdln/fomat-macros)
 use std::{
     cell::Cell,
     fmt::{self, Display, Formatter},
 };
 
-pub use yarte_derive::yformat;
+pub use yarte_derive::{yformat, yformat_html};
 pub use yarte_helpers::{helpers::Render, recompile, Error, Result};
 
-#[doc(hidden)]
+/// Closure wrapper
+///
+/// Wrap closure in mutable reference for dispatch it
 pub struct DisplayFn<F: FnOnce(&mut Formatter) -> fmt::Result>(std::cell::Cell<Option<F>>);
 
 impl<F: FnOnce(&mut Formatter) -> fmt::Result> DisplayFn<F> {
@@ -19,6 +21,7 @@ impl<F: FnOnce(&mut Formatter) -> fmt::Result> DisplayFn<F> {
     }
 }
 
+// Remove double replace in favor of single by cell::Cell::take
 impl<F: FnOnce(&mut Formatter) -> fmt::Result> Display for DisplayFn<F> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         self.0.take().ok_or(fmt::Error).and_then(|cl| cl(f))
