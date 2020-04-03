7d474566c10d4a9bfbf2f1dcbd17c9f1c3a9b7d0
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-04-03 02:22:13 +0200

    Update benches and add io::Write

--- benches/src/all.rs
+++ benches/src/all.rs
@@ -1,5 +1,8 @@
-use criterion::{criterion_group, criterion_main, Criterion};
 use std::fmt::{Display, Formatter, Result, Write};
+use std::io;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
 use yarte::Template;
 
 criterion_group!(benches, functions);
@@ -7,9 +10,11 @@ criterion_main!(benches);
 
 fn functions(c: &mut Criterion) {
     c.bench_function("Teams", teams);
+    c.bench_function("Teams io writer implements io::Write", teams_io_writer);
     c.bench_function("Teams Unescaped", teams_display);
     c.bench_function("Formatter Teams", teams_fmt);
     c.bench_function("Big table", |b| big_table(b, 100));
+    c.bench_function("Big table io writer", |b| big_table_io_writer(b, 100));
     c.bench_function("Big table Unescaped", |b| big_table_display(b, 100));
     c.bench_function("Formatter Big table", |b| big_table_fmt(b, 100));
 }
@@ -69,7 +74,7 @@ fn big_table_display(b: &mut criterion::Bencher, size: usize) {
 }
 
 #[derive(Template)]
-#[template(path = "big-table.hbs", assured = true)]
+#[template(path = "big-table.hbs", mode = "text")]
 struct BigTableDisplay {
     table: Vec<Vec<usize>>,
 }
@@ -133,7 +138,7 @@ fn teams_display(b: &mut criterion::Bencher) {
 }
 
 #[derive(Template)]
-#[template(path = "teams.hbs", assured = true)]
+#[template(path = "teams.hbs", mode = "text")]
 struct TeamsDisplay {
     year: u16,
     teams: Vec<Team>,
@@ -177,3 +182,82 @@ impl Display for TeamsFmt {
         f.write_str("</ul></body></html>")
     }
 }
+
+fn io_writer_big_table<W: std::io::Write>(f: &mut W, table: &Vec<Vec<usize>>) -> std::io::Result<()> {
+    f.write_all(b"<table>")?;
+    for i in table {
+        f.write_all(b"<tr>")?;
+        for j in i {
+            f.write_all(b"<td>")?;
+            write!(f, "{}", j)?;
+            f.write_all(b"</td>")?;
+        }
+        f.write_all(b"</tr>")?;
+    }
+    f.write_all(b"</table>")
+}
+
+fn big_table_io_writer(b: &mut criterion::Bencher, size: usize) {
+    let table = build_big_table(size);
+    let mut buf = vec![];
+    let _ = io_writer_big_table(&mut buf, &table);
+    let mut buf = Vec::with_capacity(buf.len());
+    b.iter(|| {
+        let _ = io_writer_big_table(&mut buf, &table);
+    });
+}
+
+// Version of `termcolor`
+struct TeamsWriter<W>(pub W);
+
+impl<W: io::Write> io::Write for TeamsWriter<W> {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.write(buf)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
+    }
+}
+
+impl<W: std::io::Write> TeamsWriter<W> {
+    fn write_str(&mut self, s: &str) -> io::Result<()> {
+        use std::io::Write;
+        self.write_all(s.as_bytes())
+    }
+
+    #[inline]
+    fn io_writer_teams(&mut self, Teams { year, teams }: &Teams) -> std::io::Result<()> {
+        use std::io::Write;
+        self.write_str("<html><head><title>")?;
+        write!(self, "{}", year)?;
+        self.write_str("</title></head><body><h1>CSL ")?;
+        write!(self, "{}", year)?;
+        self.write_str("</h1><ul>")?;
+        for (i, v) in teams.iter().enumerate() {
+            self.write_str("<li class=\"")?;
+            if i == 0 {
+                self.write_str("champion")?;
+            }
+            self.write_str("\"><b>")?;
+            write!(self, "{}", v.name)?;
+            self.write_str("</b>: ")?;
+            write!(self, "{}", v.score)?;
+            self.write_str("</li>")?;
+        }
+        self.write_str("</ul></body></html>")
+    }
+}
+
+fn teams_io_writer(b: &mut criterion::Bencher) {
+    let teams = Teams { year: 2015, teams: build_teams() };
+    let mut buf = TeamsWriter(vec![]);
+    let _ = buf.io_writer_teams(&teams);
+    let mut buf = TeamsWriter(Vec::with_capacity(buf.0.len()));
+    b.iter(|| {
+        let _ = buf.io_writer_teams(&teams);
+    });
+}
+
