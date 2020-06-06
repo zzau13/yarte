a4c6066bfca734707be374409dd5bfde2f167eca
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-06-06 07:23:19 +0000

    Improve fixed write performance

--- yarte_codegen/src/fixed.rs
+++ yarte_codegen/src/fixed.rs
@@ -25,12 +25,12 @@ impl<'a, T: CodeGen> FixedCodeGen<'a, T> {
                     let buf_ptr = buf.as_mut_ptr();
                     let mut buf_cur = 0;
                     macro_rules! __yarte_write_bytes {
-                        ($b:expr, $len:expr) => {
-                            if buf.len() < buf_cur + $len{
+                        ($b:ident) => {
+                            if buf.len() < buf_cur + $b.len() {
                                 return None;
                             } else {
-                                for i in $b {
-                                    buf_ptr.add(buf_cur).write(*i);
+                                for cur in 0..$b.len() {
+                                    buf_ptr.add(buf_cur).write($b.as_ptr().add(cur).read());
                                     buf_cur += 1;
                                 }
                             }
@@ -69,7 +69,11 @@ impl CodeGen for TextFixedCodeGen {
                 Local(a) => quote!(#a),
                 Lit(a) => {
                     let len = a.len();
-                    quote!(__yarte_write_bytes!(#a.as_bytes(), #len);)
+                    let a = a.into_bytes();
+                    quote! {{
+                        const YARTE_SLICE: [u8; #len] = [#(#a),*];
+                        __yarte_write_bytes!(YARTE_SLICE);
+                    }}
                 },
                 Safe(a) | Expr(a) => quote!(buf_cur += #parent::RenderSafe::render(&(#a), std::slice::from_raw_parts_mut(buf_ptr.add(buf_cur), buf.len() - buf_cur))?;),
                 Each(a) => self.gen_each(*a),
@@ -92,7 +96,11 @@ where
             Local(a) => quote!(#a),
             Lit(a) => {
                 let len = a.len();
-                quote!(__yarte_write_bytes!(#a.as_bytes(), #len);)
+                let a = a.into_bytes();
+                quote! {{
+                        const YARTE_SLICE: [u8; #len] = [#(#a),*];
+                        __yarte_write_bytes!(YARTE_SLICE);
+                    }}
             },
             Safe(a) => quote!(buf_cur += #parent::RenderSafe::render(&(#a), std::slice::from_raw_parts_mut(buf_ptr.add(buf_cur), buf.len() - buf_cur))?;),
             Expr(a) => quote!(buf_cur += #parent::RenderFixed::render(&(#a), std::slice::from_raw_parts_mut(buf_ptr.add(buf_cur), buf.len() - buf_cur))?;),
