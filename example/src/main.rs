1b7392fc92965085efcb5b3bdbc0736303f14c51
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-06-11 05:33:55 +0000

    Add example send between threads

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::uninit_assumed_init)]
 use std::collections::HashMap;
 use std::io::{stdout, Write};
-use std::mem::MaybeUninit;
+use std::thread;
 
-use yarte::{Template, TemplateFixed, TemplateMin};
+use bytes::BytesMut;
+
+use yarte::{Template, TemplateFixedMin, TemplateMin};
 
 #[derive(Template)]
 #[template(path = "index")]
@@ -17,7 +19,7 @@ struct IndexTemplateMin {
     query: HashMap<&'static str, &'static str>,
 }
 
-#[derive(TemplateFixed)]
+#[derive(TemplateFixedMin)]
 #[template(path = "index_fixed")]
 struct IndexTemplateF {
     query: HashMap<&'static str, &'static str>,
@@ -40,9 +42,23 @@ fn main() {
             query: query.clone()
         }
     );
-    let mut buf: [u8; 2048] = unsafe { MaybeUninit::uninit().assume_init() };
-    let size = unsafe { IndexTemplateF { query }.call(&mut buf) }.unwrap();
-    println!("\nFixed:");
-    let _ = stdout().lock().write(&buf[..size]);
-    println!()
+
+    let mut buf = BytesMut::with_capacity(2048);
+    unsafe {
+        // Maybe uninit
+        buf.set_len(2048);
+        let size = IndexTemplateF { query }.call(&mut buf).unwrap();
+        // bound init data
+        buf.set_len(size);
+    }
+    // Freeze
+    let buf = buf.freeze();
+    // Send to another thread
+    thread::spawn(move || {
+        println!("\nFixed Min:");
+        let _ = stdout().lock().write(&buf);
+        println!()
+    })
+    .join()
+    .unwrap();
 }
