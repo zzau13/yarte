0c96548c4490ef93f466bb0a21e6e36f40c83cb3
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-07-17 00:26:24 +0000

    Reduce size of example wasm 37K gzipped

--- example_wasm/client/src/lib.rs
+++ example_wasm/client/src/lib.rs
@@ -1,3 +1,9 @@
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::HtmlInputElement;
 
