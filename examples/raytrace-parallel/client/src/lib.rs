dcc7a027b226c515702d5dabb3a92e4361ce603a
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-09-28 16:22:08 +0000

    Add example parallel raytracing in wasm

---
+++ examples/raytrace-parallel/client/src/lib.rs
@@ -0,0 +1,77 @@
+#![feature(extern_types)]
+#![allow(clippy::manual_non_exhaustive)]
+use std::fmt;
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{Event, HtmlInputElement};
+use yarte_wasm_app::{run, App, A};
+
+use utils::console_log;
+
+use crate::app::RayTracing;
+use crate::handler::{
+    change_concurrency, enable_interface, end_render, error, paint, start_render, unsafe_paint,
+    update_time,
+};
+use crate::scene::{Img, UnsafeImg};
+
+mod app;
+mod handler;
+mod scene;
+
+enum Msg {
+    ChangeConcurrency(String),
+    EndRender(f64),
+    Error(Box<dyn fmt::Display>),
+    Paint(Img),
+    StartRender,
+    UnsafePaint(UnsafeImg),
+    UpdateTime(f64),
+}
+
+impl App for RayTracing {
+    type BlackBox = ();
+    type Message = Msg;
+
+    fn __hydrate(&mut self, addr: A<Self>) {
+        let cl = Closure::wrap(Box::new(move |event: Event| {
+            event.prevent_default();
+            addr.send(Msg::StartRender)
+        }) as Box<dyn Fn(Event)>);
+        self.button
+            .add_event_listener_with_callback("click", cl.as_ref().unchecked_ref())
+            .unwrap();
+        cl.forget();
+
+        let cl = Closure::wrap(Box::new(move |event: Event| {
+            let target = event.target().unwrap().unchecked_into::<HtmlInputElement>();
+            addr.send(Msg::ChangeConcurrency(target.value()))
+        }) as Box<dyn Fn(Event)>);
+        self.concurrency
+            .add_event_listener_with_callback("input", cl.as_ref().unchecked_ref())
+            .unwrap();
+        cl.forget();
+        self.button.set_inner_text("Render!");
+        enable_interface(self);
+        console_log!("wasm app is ready");
+    }
+
+    fn __dispatch(&mut self, msg: Self::Message, addr: A<Self>) {
+        match msg {
+            Msg::ChangeConcurrency(val) => change_concurrency(self, val),
+            Msg::EndRender(start) => end_render(self, start),
+            Msg::Error(s) => error(self, s),
+            Msg::Paint(image) => paint(self, image),
+            Msg::StartRender => start_render(self, addr),
+            Msg::UnsafePaint(image) => unsafe { unsafe_paint(self, image) },
+            Msg::UpdateTime(start) => update_time(self, start),
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    console_error_panic_hook::set_once();
+    let _addr = run!(RayTracing);
+}
---
+++ examples/raytrace-parallel/index.js
@@ -0,0 +1,36 @@
+// First up, but try to do feature detection to provide better error messages
+function loadWasm() {
+  let msg = 'This demo requires a current version of Firefox (e.g., 79.0)';
+  if (typeof SharedArrayBuffer !== 'function') {
+    alert('this browser does not have SharedArrayBuffer support enabled' + '\n\n' + msg);
+    return
+  }
+  // Test for bulk memory operations with passive data segments
+  //  (module (memory 1) (data passive ""))
+  const buf = new Uint8Array([0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00,
+    0x05, 0x03, 0x01, 0x00, 0x01, 0x0b, 0x03, 0x01, 0x01, 0x00]);
+  if (!WebAssembly.validate(buf)) {
+    alert('this browser does not support passive wasm memory, demo does not work' + '\n\n' + msg);
+    return
+  }
+
+  wasm_bindgen('./client_bg.wasm')
+      .then(start)
+      .catch(console.error);
+}
+
+loadWasm();
+
+function start() {
+  wasm_bindgen.start()
+}
+
+// TODO: will do in wasm
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function updateImage(data) {
+  canvas.width = data.width;
+  canvas.height = data.height;
+  ctx.putImageData(data, 0, 0);
+}
