98762cfbd2023c3fe0c54899ede6c12ebb26aeb0
Juan Aguilar Santillana <mhpoin@gmail.com>
2020-10-09 18:36:51 +0000

    Move parser should panic coverage

--- yarte_parser/tests/fixtures.rs
+++ yarte_parser/tests/fixtures.rs
@@ -12,10 +12,6 @@ fn read_file<P: AsRef<Path>>(path: P) -> Result<String, io::Error> {
     Ok(s.trim_end().to_string())
 }
 
-fn read_fixture(src: &str) -> Option<Vec<Fixture>> {
-    ron::from_str(src).map_err(|e| eprintln!("{:?}", e)).ok()
-}
-
 #[derive(Debug, Deserialize)]
 struct Fixture<'a> {
     #[serde(borrow)]
@@ -24,16 +20,36 @@ struct Fixture<'a> {
     exp: Vec<SNode<'a>>,
 }
 
+#[derive(Debug, Deserialize)]
+struct FixturePanic<'a>(#[serde(borrow)] &'a str);
+
 #[test]
 fn test_fixtures() {
-    for entry in glob("./tests/fixtures/**/*.ron").expect("Failed to read glob pattern") {
+    for entry in glob("./tests/fixtures/features/**/*.ron").expect("Failed to read glob pattern") {
         let name = entry.expect("File name");
-        let file = read_file(name).expect("Valid file");
-        let fixtures = read_fixture(&file).expect("Valid Fixtures");
+        let src = read_file(name).expect("Valid file");
+        let fixtures: Vec<Fixture> = ron::from_str(&src)
+            .map_err(|e| eprintln!("{:?}", e))
+            .expect("Valid Fixtures");
 
         for Fixture { src, exp } in fixtures {
             let res = parse(Cursor { rest: src, off: 0 }).expect("Valid parse");
             assert_eq!(res, exp);
+        }
+    }
+}
+
+#[test]
+fn test_fixtures_panic() {
+    for entry in glob("./tests/fixtures/panic/**/*.ron").expect("Failed to read glob pattern") {
+        let name = entry.expect("File name");
+        let src = read_file(name).expect("Valid file");
+        let fixtures: Vec<FixturePanic> = ron::from_str(&src)
+            .map_err(|e| eprintln!("{:?}", e))
+            .expect("Valid Fixtures");
+
+        for FixturePanic(src) in fixtures {
+            assert!(parse(Cursor { rest: src, off: 0 }).is_err());
         }
     }
 }
