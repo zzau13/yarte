064416af9c2604db783e53c44e1be523a65e3866
Juan Aguilar Santillana <mhpoin@gmail.com>
2021-06-16 02:34:14 +0200

    Add to CI #224 case

--- yarte/tests/issues.rs
+++ yarte/tests/issues.rs
@@ -1,4 +1,6 @@
+use std::fs::read_to_string;
 use yarte::Template;
+use yarte_helpers::config::Config;
 
 #[derive(Template)]
 #[template(src = "<h1>{{ content }}</h1>")]
@@ -10,4 +12,21 @@ struct I125 {
 fn i_125() {
     let t = I125 { content: '<' };
     assert_eq!("<h1>&lt;</h1>", t.call().unwrap());
+}
+
+#[derive(Template)]
+#[template(path = "issues/issue-224")]
+struct I224 {
+    url: String,
+}
+
+#[test]
+fn i_224() {
+    let t = I224 {
+        url: "foo".to_string(),
+    };
+    let conf = Config::new("");
+    let path = conf.get_dir().clone().join("issues/issue-224.txt");
+    let expect = read_to_string(path).unwrap();
+    assert_eq!(expect.trim(), t.call().unwrap());
 }
